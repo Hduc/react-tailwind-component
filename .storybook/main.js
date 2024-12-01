0ec35d71985243ce9fb163495612fada6bc69d32
Hoàng Đức <nghoangducit@gmail.com>
2024-12-01 22:21:58 +0700

    update storybook

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    stories: ['../stories/**/*.stories.tsx'],  // Đường dẫn đến các file stories
-    addons: [
-      '@storybook/addon-docs',  // Thêm addon để hiển thị docs và mã nguồn
-      '@storybook/addon-controls',  // Thêm addon để hiển thị controls
-    ],
-  };
-  
